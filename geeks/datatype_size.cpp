97a33fcb22978f6247b0bb408654ee55298119cc
duanweidong <misslzyan108@gmail.com>
2020-01-21 19:27:53 +0800

    add datatype_size

---
+++ geeks/datatype_size.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout << "Size of char :" << sizeof(char) << " byte" << endl;
+  cout << "Size of short int : " << sizeof(short int) << " byte" << endl;
+  cout << "Size of unsigned short int :" << sizeof(unsigned short int) << " byte" << endl;
+  cout << "Size of int :" << sizeof(int) << " byte" << endl;
+  cout << "Size of unsigned int : " << sizeof(unsigned int) << " byte" << endl;
+  cout << "Size of long int : " << sizeof(long int) << " byte" << endl;
+  cout << "Size of long long int :" << sizeof (long long int) << " byte" << endl;
+  cout << "Size of unsigned long long int : " << sizeof(unsigned long long int) <<  " byte" << endl;
+  cout << "Size of signed char :" << sizeof(signed char) << " byte" << endl;
+  cout << "Size of unsigned char :" << sizeof(unsigned char) << " byte" << endl;
+  cout << "Size of float :" << sizeof(float) << " byte" << endl;
+  cout << "Size of double :" << sizeof(double) << " byte" << endl;
+  cout << "Size of long double :" << sizeof(long double) << " byte" << endl;
+  cout << "Size of wchar_t :" << sizeof(wchar_t) << " byte" << endl;
+  return 0;
+}
