c61738c6880fc76bb0d131538af302b3ed09e8df
duanweidong <misslzyan108@gmail.com>
2020-01-22 14:11:36 +0800

    add preprocessor.c

--- geeks/preprocessor.c
+++ geeks/preprocessor.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #define LIMIT 5
+
+//define method
+#define AREA(a, b) ((a)*(b))
 int main(){
   for (int i=0;i<LIMIT;i++){
     printf("%d\n", i);
   }
+  int l1 = 10, l2 = 11, area;
+  area = AREA(l1, l2);
+  printf("area is : %d\n", area);
   return 0;
 }
--- geeks/preprocessor.cpp
+++ geeks/preprocessor.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 #define LIMIT 5
+
+// define area
+#define AREA(a, b) ((a) * (b))
 int main(){
   for(int i=0;i<LIMIT;i++){
     cout << i << "\n";
   }
-  cout << endl;
+  int a = 10, b = 11, area;
+  area = AREA(a, b);
+  cout << "area is : " << area << endl; 
   return 0;
 }
