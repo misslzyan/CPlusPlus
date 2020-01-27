e7a2dbb5ea63f8f587383585f5531f38f9c98799
duanweidong <misslzyan108@gmail.com>
2020-01-27 20:28:31 +0800

    add pointer_str_fun

--- primer/function/pointer_fun.cpp
+++ primer/function/pointer_fun.cpp
@@ -20,10 +20,30 @@ int (*init1())[10] {
   return &a;
 }
 
+auto init2() -> int(*)[10] {
+  static int a[10] = {0};
+  return &a;
+}
+
+typedef int arrT[10];
+using arrtT = int[10];
+arrtT* func(int i) {
+  static int a[10] = {0};
+  return &a;
+}
+
 int main () {
  for(auto c :  *(init())) {
    cout << c << " ";
  }
  cout << endl;
-  init1();
+ init1();
+ for (auto c : *init2()) {
+   cout << c << " ";
+ }
+ cout << endl;
+ for (auto c : *func(1)) {
+   cout << c << " ";
+ }
+ cout << endl;
 }
