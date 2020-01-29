99fbd1665b4920eaf945aebecdb2f5c4977a49ea
duanweidong <misslzyan108@gmail.com>
2020-01-29 11:26:06 +0800

    add template

--- primer/template/include/template.h
+++ primer/template/include/template.h
@@ -5,11 +5,21 @@
 #include <string>
 
 namespace dwd {
-template <typename T>
+  template <typename T>
   int compare(const T &v1, const T &v2) {
     if (v1 < v2 ) return -1;
     if (v1 > v2 ) return 1;
     return 0;
+  }
+  template <typename T>
+  inline T min(const T &v1, const T &v2) {
+    return v1 < v2 ? v1 : v2;
+  }
+  
+  //compare only string.
+  template <unsigned M, unsigned N>
+  int compare_string(const char (&p1)[M], const char (&p2)[N]) {
+    return strcmp(p1, p2);
   }
 }
 
--- primer/template/templateDemo.cpp
+++ primer/template/templateDemo.cpp
@@ -3,5 +3,8 @@ using namespace std;
 int main () {
   cout << dwd::compare(2, 1) << endl;
   cout << dwd::compare("aa", "bb") << endl;
+  cout << dwd::compare_string("aa", "bb") << endl;
+  cout << dwd::min(2, 1) << endl;
+  cout << dwd::min(4, 5) << endl;
   return 0;
 }
