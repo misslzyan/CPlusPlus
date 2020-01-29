4295b93128bb2a2fb9b481dd233a7dd7bbc5d07a
duanweidong <misslzyan108@gmail.com>
2020-01-29 14:00:29 +0800

    add int_random

--- primer/random/int_random.cpp
+++ primer/random/int_random.cpp
@@ -6,6 +6,34 @@ void printRandom(T &r) {
     std::cout << r() << " ";
   }
   std::cout << "\n";
+  std::cout << "Max is :" << r.max() << std::endl;
+  std::cout << "Min is :" << r.min() << std::endl;
+}
+
+void print(int min, int max, int (*fun)(int, int)) {
+  for (unsigned i = 0; i != 5; i++) {
+    std::cout << fun(min, max) << " ";
+  }
+  std::cout << std::endl;
+}
+
+template <typename T, typename M>
+void printR(T &r, M &m) {
+  for (unsigned i = 0; i != 5; i++) {
+    std::cout << r(m) << " ";
+  }
+  std::cout << "\n";
+}
+
+/*
+  generate a random int between min and max.
+*/
+int generateInt(int min, int max) {
+  // use for generating random seed.
+  std::random_device rd;
+  std::mt19937 mt(rd());//generate a random with a seed.
+  std::uniform_int_distribution<int> distribution(min, max);
+  return distribution(mt);
 }
 
 int main () {
@@ -23,5 +51,11 @@ int main () {
   // generate the different random int.
   std::default_random_engine r2(10);
   printRandom(r2);
+  
+  // random 0 ~ 9
+  std::uniform_int_distribution<int> u(0, 9);
+  printR(u, r);
+  printR(u, rd);
+  print(0, 9,generateInt);
   return 0;
 }
