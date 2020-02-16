8fabda5fd337fd9e0ecbd859fcf52752cd5079b9
duanweidong <misslzyan108@gmail.com>
2020-02-16 20:54:35 +0800

    add sort

--- primer/Generic_Algorithms_10/sort.cpp
+++ primer/Generic_Algorithms_10/sort.cpp
@@ -2,6 +2,13 @@
 #include <string>
 #include <vector>
 #include <sstream>
+
+void elimitWords(std::vector<std::string> &words) {
+  std::sort(words.begin(), words.end());
+  auto beginToErase = std::unique(words.begin(), words.end());
+  words.erase(beginToErase, words.end());
+}
+
 int main () {
   std::string input_str = "the quick red fox jumps over the slow red turtle";
   std::string word;
@@ -14,5 +21,11 @@ int main () {
   for(auto w : words) {
     std::cout << w << " "; 
    }
+  elimitWords(words);
   std::cout << "\n";
+  for(auto w : words) {
+    std::cout << w << " ";
+  }
+  std::cout << "\n";
+  return 0;
 }
