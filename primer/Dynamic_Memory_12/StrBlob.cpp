cc0344c1102eb94704b34eaec5c0c7745bb61aef
duanweidong <duanweidong@meituan.com>
2020-02-21 22:49:33 +0800

    add StrBlob

---
+++ primer/Dynamic_Memory_12/StrBlob.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+#include <string>
+#include <memory>
+#include <iostream>
+class StrBlob {
+public:
+  typedef std::vector<std::string>::size_type size_type;
+  StrBlob();
+  StrBlob(std::initializer_list<std::string> il);
+  size_type size() const { return data -> size();}
+  bool empty() const { return data -> empty();}
+  //add and remove elements.
+  void push_back(const std::string &t) {
+    data -> push_back(t);
+  }
+  void pop_back();
+  //elements access.
+  std::string & front();
+  std::string & back();
+private:
+  std::shared_ptr<std::vector<std::string>> data;
+  // throws msg if data[i] isn't valid.
+  void check(size_type i, const std::string &msg) const;
+};
+
+StrBlob::StrBlob():data(std::make_shared<std::vector<std::string>>()) {}
+
+StrBlob::StrBlob(std::initializer_list<std::string> il): data(std::make_shared<std::vector<std::string>>(il)) {}
+
+void StrBlob::check(size_type i, const std::string &msg) const {
+  if(i >= data -> size()){
+    throw std::out_of_range(msg);
+  }
+}
+
+void StrBlob::pop_back() {
+  check(0, "pop_back on empty StrBlob");
+  data -> pop_back();
+}
+
+std::string & StrBlob::front() {
+  check(0, "front on empty StrBlob");
+  return data -> front();
+}
+
+std::string & StrBlob::back() {
+  check(0, "back on empty StrBlob");
+  return data -> back();
+}
+
+int main() {
+  StrBlob blob = {"1", "2", "23"};
+  auto b = blob;
+  std::cout << b.front() << "\n";
+}
