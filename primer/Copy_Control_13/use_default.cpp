ca4c8c36e7f62ca703aa51d8d9525ecff7df49b2
duanweidong <duanweidong@meituan.com>
2020-07-03 12:48:09 +0800

    add use_default

---
+++ primer/Copy_Control_13/use_default.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class Sales_data {
+  Sales_data() = default;
+  Sales_data(const Sales_data &) = default;
+  Sales_data& operator=(const Sales_data &) = default;
+  ~Sales_data() = default;
+};
+
+int main () {
+
+}
